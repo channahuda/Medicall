97d7a42dd4220c1ced3042002337abae370be158
MahumFatimaKhan <mfkmahum@gmail.com>
2022-04-25 01:02:20 +0500

    firebase setup

--- project/lib/View/main.dart
+++ project/lib/View/main.dart
@@ -1,11 +1,14 @@
 import 'dart:async';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:medicall/View/HomePage.dart';
 
 import 'Login.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(
     MyApp(),
   );
