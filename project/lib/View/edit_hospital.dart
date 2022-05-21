930b44c43d704b35871650eb80f71343339ec592
SafaAbdulHai <s.hai_18625@khi.iba.edu.pk>
2022-05-21 23:18:03 +0500

    Revert "remove back button from edit hospital"
    This reverts commit 2fb1dd151c09c3773fda5e4688471fc4abf7a4ef.

--- project/lib/View/edit_hospital.dart
+++ project/lib/View/edit_hospital.dart
@@ -55,7 +55,11 @@ class _EditHospitalState extends State<EditHospital> {
               //because overflow occurs when keyboard pops up
               backgroundColor: const Color(0xffF8F8F8),
               appBar: AppBar(
-                  automaticallyImplyLeading : false,
+                leading: IconButton(
+                    icon: const Icon(Icons.arrow_back),
+                    onPressed: () {
+                      Navigator.pop(context);
+                    }),
                 backgroundColor: const Color(0xFF353559),
                 centerTitle: true,
                 title: Text(
